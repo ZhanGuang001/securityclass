a04b46cc470ddba9e60045fd27c515f44a4906e2
qiao <QiaoZhanguang@163.com>
2019-08-07 18:49:08 +0800

    spring security的aouth2的授权登录，github和太极

---
+++ securityday6/src/main/java/cn/com/taiji/security/securityday6/Securityday6Application.java
@@ -0,0 +1,13 @@
+package cn.com.taiji.security.securityday6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securityday6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Securityday6Application.class, args);
+    }
+
+}
